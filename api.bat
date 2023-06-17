fb8473c83d5509c56cc86f01d23f399cb3224723
rondonbrij <ronjames6123@gmail.com>
2023-06-18 04:46:58 +0800

    Added some minor details, main choice, main menu, and anchors.

--- api.bat
+++ api.bat
@@ -1,5 +1,6 @@
 @echo off
 
+start "Customer Management System" cmd /k "python api_finals.py"
 
 echo.
 echo   _____         __                       ________  __  _____ 
@@ -8,4 +9,30 @@ echo / /__/ // ^(_-^</ __/ _ \/  ' \/ -_^) __/ / /__/ , _/ /_/ / // /
 echo \___/\_,_/___/\__/\___/_/_/_/\__/_/    \___/_/^|_^|\____/____/ 
 echo.
 
-pause
+
+curl http://127.0.0.1:5000
+set /p choice=Enter your choice (1-4 or e): 
+
+if "%choice%"=="1" (
+	cls
+	goto add
+) else if "%choice%"=="2" (
+	cls
+	goto retrieve
+) else if "%choice%"=="3" (
+	cls
+	goto update
+) else if "%choice%"=="4" (
+	cls
+	goto delete
+) else if "%choice%"=="e" (
+	cls
+	goto end
+)
+
+
+:add
+:retrieve
+:update
+:delete
+:end
